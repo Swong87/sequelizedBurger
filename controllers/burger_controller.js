575e8b33fb038528fde4cad3ad2338e3fe669619
Swong87 <8swordz@gmail.com>
2017-08-16 11:27:56 -0400

    sequelized

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,48 +1,68 @@
-var express = require("express");
 
-var router = express.Router();
+var db = require("../models");
 
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+module.exports = function(app) {
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    // console.log(hbsObject);
-    res.render("index", hbsObject);
+  // GET route for getting all of the burgers
+  app.get("/", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Burger.findAll({}).then(function(dbBurger) {
+      // We have access to the burgers as an argument inside of the callback function
+      // res.json(dbBurger);
+      var hbsObject = {
+        burgers: dbBurger
+      };
+
+      res.render("index", hbsObject);
+    });
+
   });
-});
 
-router.post("/", function(req, res) {
-  burger.create(["burger_name"], [
-    req.body.name
-  ], function() {
-    res.redirect("/");
+  // POST route for saving a new burger
+  app.post("/", function(req, res) {
+    // create takes an argument of an object describing the item we want to insert
+    // into our table. In this case we just we pass in an object with a text and
+    // complete property
+    db.Burger.create({
+      burger_name: req.body.name
+    }).then(function(dbBurger) {
+      // We have access to the new burger as an argument inside of the callback function
+      // res.json(dbBurger);
+      res.redirect("/");
+    });
+
   });
-});
 
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  // DELETE route for deleting burgers. We can get the id of the burger to be deleted
+  // from req.params.id
+  app.delete("/:id", function(req, res) {
+    // Destroy takes in one argument: a "where object describing the burgers we want to destroy
+    db.Burger.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbBurger) {
+      // res.json(dbBurger);
+      res.redirect("/");
+    });
 
-  // console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/");
   });
-});
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+  // PUT route for updating burgers. We can get the updated burger data from req.body
+  app.put("/:id", function(req, res) {
+    var condition = req.params.id;
+    db.Burger.update({
+      devoured: req.body.devoured
+    }, {
+      where: {
+        id: condition
+      }
+    })
+    .then(function() {
+      // res.json(dbBurger);
+      res.redirect("/");
+    });
 
-  burger.delete(condition, function() {
-    res.redirect("/");
   });
-});
-
-// Export routes for server.js to use.
-module.exports = router;
+};
--- server.js
+++ server.js
@@ -3,13 +3,18 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var PORT = process.env.PORT || 3000;
-
 var app = express();
+
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -21,10 +26,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller.js");
+require("./controllers/burger_controller.js")(app);
 
-app.use("/", routes);
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
